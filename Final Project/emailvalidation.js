811657d9ebf15640347c9585de0a948e06860364
gnpthbalaji <59932973+gnpthbalaji@users.noreply.github.com>
2020-02-20 01:20:17 -0500

    daassa

---
+++ Final Project/emailvalidation.js
@@ -0,0 +1,9 @@
+function validateEmail(sEmail) {
+    var reEmail = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+  
+    if(!sEmail.match(reEmail)) {
+      alert("Please enter a valid Email Address");
+      return false;
+    }
+    return true;
+  }
